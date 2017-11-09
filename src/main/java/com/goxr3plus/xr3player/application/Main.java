f57b69ffffcb4c763cf0a717f495fb583878dfc4
GOXR3PLUS <crAlexcomp@gmail.com>
2017-11-10 00:58:42 +0200

    Fixed Bug with Autoupdater

--- src/main/java/com/goxr3plus/xr3player/application/Main.java
+++ src/main/java/com/goxr3plus/xr3player/application/Main.java
@@ -101,8 +101,8 @@ public class Main extends Application {
 	public static Properties internalInformation = new Properties();
 	static {
 		//----------Properties-------------
-		internalInformation.put("Version", 87);
-		internalInformation.put("ReleasedDate", "?/11/2017");
+		internalInformation.put("Version", 90);
+		internalInformation.put("ReleasedDate", "13/11/2017");
 		
 		System.out.println("Outside of Application Start Method");
 	}
