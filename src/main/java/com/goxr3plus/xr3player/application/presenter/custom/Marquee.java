b2ea3a83260472ab6ab347e4a8f44cdcd685ae2b
GOXR3PLUS STUDIO <crAlexcomp@gmail.com>
2017-12-09 02:05:00 +0200

    Code format

--- src/main/java/com/goxr3plus/xr3player/application/presenter/custom/Marquee.java
+++ src/main/java/com/goxr3plus/xr3player/application/presenter/custom/Marquee.java
@@ -24,126 +24,126 @@ import main.java.com.goxr3plus.xr3player.application.tools.InfoTool;
  *
  */
 public class Marquee extends Pane {
-
-    @FXML
-    private Text text;
-
-    // minimum distance to Pane bounds
-    private static final double OFFSET = 5;
-
-    private Timeline timeline = new Timeline();
-
-    /**
-     * Constructor
-     */
-    public Marquee() {
-
-	// FXMLLOADER
-	try {
-	    FXMLLoader loader = new FXMLLoader(getClass().getResource(InfoTool.FXMLS + "Marquee.fxml"));
-	    loader.setController(this);
-	    loader.setRoot(this);
-	    loader.load();
-	} catch (IOException ex) {
-	    ex.printStackTrace();
-	}
-
-    }
-
-    /**
-     * Called as soon as .fxml is initialized [[SuppressWarningsSpartan]]
-     */
-    @FXML
-    private void initialize() {
-
-	//Clip
-	Rectangle rectangle = new Rectangle(25, 25);
-	rectangle.widthProperty().bind(widthProperty());
-	rectangle.heightProperty().bind(heightProperty());
-	setClip(rectangle);
-
-	// Text
-	text.setManaged(false);
-
-	startAnimation();
-    }
-
-    /**
-     * This method changes the text of the Marquee
-     * 
-     * @param value
-     * @return this
-     */
-    public Marquee setText(String value) {
-
-	// text
-	text.setText(value);
-
-	return this;
-    }
-
-    /**
-     * Defines text string that is to be displayed.
-     * 
-     * @return The TextProperty
-     */
-    public StringProperty textProperty() {
-	return text.textProperty();
-    }
-
-    /**
-     * This method starts the Animation of the marquee
-     */
-    private final void startAnimation() {
-
-	// KeyFrame
-	KeyFrame updateFrame = new KeyFrame(Duration.millis(35), new EventHandler<ActionEvent>() {
-
-	    private boolean rightMovement;
-
-	    @Override
-	    public void handle(ActionEvent event) {
-		double textWidth = text.getLayoutBounds().getWidth();
-		double paneWidth = getWidth();
-		double layoutX = text.getLayoutX();
-
-		if (2 * OFFSET + textWidth <= paneWidth && layoutX >= OFFSET) {
-		    // stop, if the pane is large enough and the position is
-		    // correct
-		    text.setLayoutX(OFFSET);
-		    timeline.stop();
-		} else {
-		    if ((rightMovement && layoutX >= OFFSET) || (!rightMovement && layoutX + textWidth + OFFSET <= paneWidth)) {
-			// invert movement, if bounds are reached
-			rightMovement = !rightMovement;
-		    }
-
-		    // update position
-		    if (rightMovement) {
-			layoutX += 1;
-		    } else {
-			layoutX -= 1;
-		    }
-		    text.setLayoutX(layoutX);
+	
+	@FXML
+	private Text text;
+	
+	// minimum distance to Pane bounds
+	private static final double OFFSET = 5;
+	
+	private Timeline timeline = new Timeline();
+	
+	/**
+	 * Constructor
+	 */
+	public Marquee() {
+		
+		// FXMLLOADER
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(InfoTool.FXMLS + "Marquee.fxml"));
+			loader.setController(this);
+			loader.setRoot(this);
+			loader.load();
+		} catch (IOException ex) {
+			ex.printStackTrace();
 		}
-	    }
-	});
-	timeline.getKeyFrames().add(updateFrame);
-	timeline.setCycleCount(Animation.INDEFINITE);
-
-	// listen to bound changes of the elements to start/stop the
-	// animation
-	InvalidationListener listener = o -> {
-	    double textWidth = text.getLayoutBounds().getWidth();
-	    double paneWidth = getWidth();
-	    text.setLayoutX(5);
-	    if (textWidth + 2 * OFFSET > paneWidth && timeline.getStatus() != Animation.Status.RUNNING)
-		timeline.play();
-	};
-
-	text.layoutBoundsProperty().addListener(listener);
-	widthProperty().addListener(listener);
-
-    }
-
+		
+	}
+	
+	/**
+	 * Called as soon as .fxml is initialized [[SuppressWarningsSpartan]]
+	 */
+	@FXML
+	private void initialize() {
+		
+		//Clip
+		Rectangle rectangle = new Rectangle(25, 25);
+		rectangle.widthProperty().bind(widthProperty());
+		rectangle.heightProperty().bind(heightProperty());
+		setClip(rectangle);
+		
+		// Text
+		text.setManaged(false);
+		
+		startAnimation();
+	}
+	
+	/**
+	 * This method changes the text of the Marquee
+	 * 
+	 * @param value
+	 * @return this
+	 */
+	public Marquee setText(String value) {
+		
+		// text
+		text.setText(value);
+		
+		return this;
+	}
+	
+	/**
+	 * Defines text string that is to be displayed.
+	 * 
+	 * @return The TextProperty
+	 */
+	public StringProperty textProperty() {
+		return text.textProperty();
+	}
+	
+	/**
+	 * This method starts the Animation of the marquee
+	 */
+	private final void startAnimation() {
+		
+		// KeyFrame
+		KeyFrame updateFrame = new KeyFrame(Duration.millis(35), new EventHandler<ActionEvent>() {
+			
+			private boolean rightMovement;
+			
+			@Override
+			public void handle(ActionEvent event) {
+				double textWidth = text.getLayoutBounds().getWidth();
+				double paneWidth = getWidth();
+				double layoutX = text.getLayoutX();
+				
+				if (2 * OFFSET + textWidth <= paneWidth && layoutX >= OFFSET) {
+					// stop, if the pane is large enough and the position is
+					// correct
+					text.setLayoutX(OFFSET);
+					timeline.stop();
+				} else {
+					if ( ( rightMovement && layoutX >= OFFSET ) || ( !rightMovement && layoutX + textWidth + OFFSET <= paneWidth )) {
+						// invert movement, if bounds are reached
+						rightMovement = !rightMovement;
+					}
+					
+					// update position
+					if (rightMovement) {
+						layoutX += 1;
+					} else {
+						layoutX -= 1;
+					}
+					text.setLayoutX(layoutX);
+				}
+			}
+		});
+		timeline.getKeyFrames().add(updateFrame);
+		timeline.setCycleCount(Animation.INDEFINITE);
+		
+		// listen to bound changes of the elements to start/stop the
+		// animation
+		InvalidationListener listener = o -> {
+			double textWidth = text.getLayoutBounds().getWidth();
+			double paneWidth = getWidth();
+			text.setLayoutX(5);
+			if (textWidth + 2 * OFFSET > paneWidth && timeline.getStatus() != Animation.Status.RUNNING)
+				timeline.play();
+		};
+		
+		text.layoutBoundsProperty().addListener(listener);
+		widthProperty().addListener(listener);
+		
+	}
+	
 }
