e1bcd27e21b3d1636ca4092fc5ebcc14959080b0
GOXR3PLUS STUDIO <crAlexcomp@gmail.com>
2018-02-25 23:44:59 +0200

    Fixed something in Update Window

--- src/main/java/com/goxr3plus/xr3player/application/versionupdate/UpdateWindow.java
+++ src/main/java/com/goxr3plus/xr3player/application/versionupdate/UpdateWindow.java
@@ -218,9 +218,9 @@ public class UpdateWindow extends StackPane {
 	private void searchForUpdatesPart2(boolean showTheWindow) {
 		try {
 			
-			//Document doc = Jsoup.connect("https://raw.githubusercontent.com/goxr3plus/XR3Player/master/XR3PlayerUpdatePage.html").get();
+			Document doc = Jsoup.connect("https://raw.githubusercontent.com/goxr3plus/XR3Player/master/XR3PlayerUpdatePage.html").get();
 			
-			Document doc = Jsoup.parse(new File("XR3PlayerUpdatePage.html"), "UTF-8", "http://example.com/");
+			//Document doc = Jsoup.parse(new File("XR3PlayerUpdatePage.html"), "UTF-8", "http://example.com/");
 			
 			Element lastArticle = doc.getElementsByTag("article").last();
 			
