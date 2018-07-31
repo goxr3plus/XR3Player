af57194d9ae349af754ee5dd5d6b06f46854fa96
GOXR3PLUS STUDIO <crAlexcomp@gmail.com>
2018-07-31 11:01:00 +0300

    Released V3.117

--- src/main/java/com/goxr3plus/xr3player/application/Main.java
+++ src/main/java/com/goxr3plus/xr3player/application/Main.java
@@ -124,7 +124,7 @@ public class Main extends Application {
 	public static final PropertiesDb applicationProperties = new PropertiesDb(InfoTool.getAbsoluteDatabasePathWithSeparator() + "ApplicationProperties.properties", true);
 	
 	//Internal Information
-	public static final int APPLICATION_VERSION = 117;
+	public static final int APPLICATION_VERSION = 118;
 	public static final String RELEASE_DATE = "Check updates window";
 	
 	private static final Logger[] pin;
