fe590212e526320be53f311ceba3c647ab49fb06
GOXR3PLUS STUDIO <crAlexcomp@gmail.com>
2018-08-14 19:03:59 +0300

    Removed musiccot license

--- src/main/java/com/goxr3plus/xr3player/xplayer/waveform/WaveFormPane.java
+++ src/main/java/com/goxr3plus/xr3player/xplayer/waveform/WaveFormPane.java
@@ -1,11 +1,3 @@
-/*
- * This file is part of Musicott software. Musicott software is free software: you can redistribute it and/or modify it under the terms of the GNU
- * General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
- * Musicott library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public
- * License along with Musicott. If not, see <http://www.gnu.org/licenses/>. Copyright (C) 2015 - 2017 Octavio Calleya
- */
-
 package main.java.com.goxr3plus.xr3player.xplayer.waveform;
 
 import javafx.scene.paint.Color;
