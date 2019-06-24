f29472e5a5288ead280cb3433aa8f69739d5fcd6
Helge Stenström <h.stenstrom@gmail.com>
2019-06-24 23:04:57 +0200

    Parameterized tests
    Parameterized tests help with writing independent tests without too much code duplication. They are often clearer and easier to understand too.

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -6,6 +8,19 @@ public class ExampleTest {
     @Test
     void example() {
         String message ="This test is a placeholder for the directory that should hold tests. Remove it when there are real tests!";
-        assertEquals(1, 2, message);
+        assertEquals(1, 2, message);  // TODO: remove this failing test.
+    }
+
+
+    // A simple way to define a parameterized test. Not the most powerful, though.
+    @ParameterizedTest
+    @CsvSource({
+            "1, 2, 3",
+            "10, 12, 22",
+            "1, -100, -99",
+            "0, 0, 17"  // Expected to fail. TODO: Remove Failing tests should probably not be saved.
+    })
+    void parameterizedExample(int firstTerm, int secondTerm, int expectedSum) {
+    assertEquals(expectedSum, firstTerm + secondTerm);
     }
 }
