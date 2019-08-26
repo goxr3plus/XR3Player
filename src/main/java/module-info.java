420fa4672cb7bbaeb9f14ba777ba4c5031306191
Helge Stenström <h.stenstrom@gmail.com>
2019-08-26 19:08:40 +0200

    Update module-info.java
    All opens sorted in alphabetical order.
    All requires sorted in alphabetical order.
    This makes it easier to get a mental picture of what is opened and required.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,40 +1,39 @@
 module XR3Player {
 
-	opens com.goxr3plus.xr3player.controllers.general to javafx.fxml;
-	opens com.goxr3plus.xr3player.controllers.windows to javafx.fxml;
-	opens com.goxr3plus.xr3player.controllers.tagging to javafx.fxml;
-	opens com.goxr3plus.xr3player.controllers.settings to javafx.fxml;
-	opens com.goxr3plus.xr3player.controllers.custom to javafx.fxml;
-	opens com.goxr3plus.xr3player.controllers.systemtree to javafx.fxml;
-	opens com.goxr3plus.xr3player.controllers.smartcontroller to javafx.fxml;
-	opens com.goxr3plus.xr3player.controllers.moviemode to javafx.fxml;
-	opens com.goxr3plus.xr3player.controllers.xplayer to javafx.fxml;
-	opens com.goxr3plus.xr3player.controllers.djmode to javafx.fxml;
-	opens com.goxr3plus.xr3player.xplayer.visualizer.presenter to javafx.fxml;
-	opens com.goxr3plus.xr3player.controllers.librarymode to javafx.fxml;
-	opens com.goxr3plus.xr3player.controllers.loginmode to javafx.fxml;
-	opens com.goxr3plus.xr3player.controllers.dropbox to javafx.fxml;
-	opens com.goxr3plus.xr3player.controllers.chromium to javafx.fxml;
 
 	opens com.goxr3plus.xr3player.application to javafx.graphics;
+	opens com.goxr3plus.xr3player.controllers.chromium to javafx.fxml;
+	opens com.goxr3plus.xr3player.controllers.custom to javafx.fxml;
+	opens com.goxr3plus.xr3player.controllers.djmode to javafx.fxml;
+	opens com.goxr3plus.xr3player.controllers.dropbox to javafx.fxml;
+	opens com.goxr3plus.xr3player.controllers.general to javafx.fxml;
+	opens com.goxr3plus.xr3player.controllers.librarymode to javafx.fxml;
+	opens com.goxr3plus.xr3player.controllers.loginmode to javafx.fxml;
+	opens com.goxr3plus.xr3player.controllers.moviemode to javafx.fxml;
+	opens com.goxr3plus.xr3player.controllers.settings to javafx.fxml;
+	opens com.goxr3plus.xr3player.controllers.smartcontroller to javafx.fxml;
+	opens com.goxr3plus.xr3player.controllers.systemtree to javafx.fxml;
+	opens com.goxr3plus.xr3player.controllers.tagging to javafx.fxml;
+	opens com.goxr3plus.xr3player.controllers.windows to javafx.fxml;
+	opens com.goxr3plus.xr3player.controllers.xplayer to javafx.fxml;
 	opens com.goxr3plus.xr3player.models.smartcontroller to javafx.base;
+	opens com.goxr3plus.xr3player.xplayer.visualizer.presenter to javafx.fxml;
 
-	requires java.base;
-	requires java.sql;
-	requires java.logging;
 	requires FX.BorderlessScene;
-//    requires XR3Capture;
 	requires com.goxr3plus.streamplayer;
 	requires com.jfoenix;
 	requires commons.validator;
-	requires org.controlsfx.controls;
 	requires dropbox.core.sdk;
 	requires evo.inflector;
 	requires flowless;
 	requires image4j;
 	requires jaudiotagger;
+	requires java.base;
 	requires java.flac.encoder;
 	requires java.google.speech.api;
+	requires java.logging;
+	requires java.net.http;
+	requires java.sql;
 	requires javafx.base;
 	requires javafx.controls;
 	requires javafx.fxml;
@@ -48,11 +47,12 @@ module XR3Player {
 	requires jxbrowser;
 	requires mp3agic;
 	requires org.apache.commons.io;
+	requires org.controlsfx.controls;
 	requires org.jsoup;
 	requires org.kordamp.iconli.core;
 	requires org.kordamp.ikonli.javafx;
 	requires reactfx;
 	requires richtextfx;
 	requires zt.zip;
-	requires java.net.http;
+//    requires XR3Capture;
 }
